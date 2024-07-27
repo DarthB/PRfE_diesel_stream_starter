77d89c2dc1ec2efa541f0ed760ba2b50f0bc38e7
Tim Janus <tim@janus.rs>
2024-07-27 09:23:10 +0000

    feat: establishing a postgres connection

--- thermo_properties/src/main.rs
+++ thermo_properties/src/main.rs
@@ -1,6 +1,16 @@
 use std::error::Error;
 
+use diesel::{Connection, PgConnection};
+
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello World!");
+    dotenvy::from_filename("./.env")?;
+
+    let db_url = dotenvy::var("DATABASE_URL")?;
+    let res = PgConnection::establish(&db_url);
+    match res {
+        Ok(_) => println!("Connection Established"),
+        Err(e) => return Err(Box::new(e)),
+    }
+
     Ok(())
 }
