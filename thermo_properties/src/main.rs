0b3b85287fbc874e3011398f59fe5b3a7e052b6e
Tim Janus <tim@janus.rs>
2024-07-27 10:07:06 +0000

    feat: create some molecue and delete-all
    restructure diesel migration and schema

--- thermo_properties/src/main.rs
+++ thermo_properties/src/main.rs
@@ -1,15 +1,63 @@
 use std::error::Error;
 
-use diesel::{Connection, PgConnection};
+use clap::{Parser, Subcommand};
+use diesel::{Connection, PgConnection, RunQueryDsl};
+use models::Molecule;
+
+mod models;
+mod schema;
+
+#[derive(Debug, Parser)]
+struct CliArgs {
+    #[command(subcommand)]
+    sub_command: SubCommands,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum SubCommands {
+    Create,
+    Import,
+    Read,
+    Update,
+    DeleteAll,
+}
+
+fn cmd_create(name: &str, formula: &str, conn: &mut PgConnection) {
+    println!("name={}, symbol={}", name, formula);
+    let record = Molecule::new_with_mutator(name.to_owned(), formula.to_owned(), |mol| {
+        mol.boiling_point = Some(42.)
+    });
+
+    diesel::insert_into(schema::molecules::dsl::molecules)
+        .values(record)
+        .execute(conn)
+        .unwrap();
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::from_filename("./.env")?;
 
     let db_url = dotenvy::var("DATABASE_URL")?;
     let res = PgConnection::establish(&db_url);
-    match res {
-        Ok(_) => println!("Connection Established"),
+    let mut conn = match res {
+        Ok(conn) => {
+            println!("Connection Established");
+            conn
+        }
         Err(e) => return Err(Box::new(e)),
+    };
+
+    let args = CliArgs::parse();
+
+    match args.sub_command {
+        SubCommands::Create => cmd_create("my-mol", "ACDC", &mut conn),
+        SubCommands::Import => todo!(),
+        SubCommands::Read => todo!(),
+        SubCommands::Update => todo!(),
+        SubCommands::DeleteAll => {
+            diesel::delete(schema::molecules::dsl::molecules).execute(&mut conn)?;
+            println!("deleted all");
+        }
     }
 
     Ok(())
---
+++ thermo_properties/src/models.rs
@@ -0,0 +1,37 @@
+use diesel::prelude::*;
+
+use crate::schema::molecules;
+
+#[derive(Insertable, Debug, Clone, Default)]
+#[diesel(table_name=molecules)]
+pub struct Molecule {
+    #[diesel(skip_insertion)]
+    pub molecule_id: i32,
+
+    pub name: String,
+    pub formula: String,
+    pub density: Option<f64>,
+    pub molar_mass: Option<f64>,
+    pub acentric_factor: Option<f64>,
+    pub melting_point: Option<f64>,
+    pub boiling_point: Option<f64>,
+    pub critical_temperature: Option<f64>,
+    pub critical_pressure: Option<f64>,
+}
+
+impl Molecule {
+    pub fn new_with_mutator<'a, F>(name: String, formula: String, mutator: F) -> Self
+    where
+        F: Fn(&mut Molecule),
+    {
+        let mut reval = Molecule {
+            name,
+            formula,
+            ..Default::default()
+        };
+
+        mutator(&mut reval);
+
+        reval
+    }
+}
